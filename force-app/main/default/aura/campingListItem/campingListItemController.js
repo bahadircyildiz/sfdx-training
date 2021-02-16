89cb883367cb38f6b9c3e0a602a7412641e9946a
Bahadir Can Yildiz <bahadircanyildiz@gmail.com>
2021-02-16 18:52:09 +0300

    [lex_dev_lc_basics_controllers] Added onclick event to a button, intro to controllers

--- force-app/main/default/aura/campingListItem/campingListItemController.js
+++ force-app/main/default/aura/campingListItem/campingListItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
+  packItem: function (component, event, handler) {
+    var btnClicked = event.getSource();
 
-    }
-})
+    btnClicked.set("v.disabled", true);
+    component.set("v.item.Packed__c", true);
+  }
+});
